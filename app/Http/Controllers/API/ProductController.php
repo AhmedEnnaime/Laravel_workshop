a0f6949c0ca53e5e372f9052483017ff04a8eecc
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-02-12 18:40:45 +0100

    feat: :white_check_mark: added product crud

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -1,11 +1,75 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends BaseController
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+
+        return $this->sendResponse(ProductResource::collection($products), 'Products retrieved successfully.', 200);
+    }
+
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = FacadesValidator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $product = Product::create($input);
+
+        return $this->sendResponse(new ProductResource($product), 'Product created successfully.', 201);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (is_null($product)) {
+            return $this->sendError('Product not found.');
+        }
+
+        return $this->sendResponse(new ProductResource($product), 'Product retrieved successfully.', 200);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $input = $request->all();
+
+        $validator = FacadesValidator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $product->name = $input['name'];
+        $product->detail = $input['detail'];
+        $product->save();
+
+        return $this->sendResponse(new ProductResource($product), 'Product updated successfully.', 200);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return $this->sendResponse([], 'Product deleted successfully.', 202);
+    }
 }
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -16,7 +16,7 @@ class UserController extends BaseController
         $validator = FacadesValidator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email',
-            'password' => 'required',
+            'password' => 'required|min:6',
             'c_password' => 'required|same:password',
         ]);
 
